a756acbf687c96a3f7f296468bfe253d3ea638b0
Devan Everette <73131489+DEverette11@users.noreply.github.com>
2023-04-18 11:22:17 -0500

    Completed Game Functionality
    Finished developing the base functionality of the game with the projectile knocking down the castle, and touching the goal changes the color of the goal

--- Assets/__Scripts/RigidbodySleep.cs
+++ Assets/__Scripts/RigidbodySleep.cs
@@ -2,17 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent (typeof(Rigidbody))]
 public class RigidbodySleep : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    private int sleepCountdown = 4;
+    private Rigidbody rigid;
+
+    void Awake() {
+        rigid = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    void FixedUpdate() {
+        if(sleepCountdown > 0) {
+            rigid.Sleep();
+            sleepCountdown--;
+        }
     }
 }
